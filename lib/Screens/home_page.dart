faffbf4c6df827882586cb167dab592ef63c5867
Assassin33GT <bodeidris33@gmail.com>
2025-09-12 18:54:35 +0300

    google_fonts

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Text("HomePage")]);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Center(child: Text("HomePage", style: GoogleFonts.voces(fontSize: 30))),
+      ],
+    );
   }
 }
