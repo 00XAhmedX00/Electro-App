0381baba73bca7a0519bb4828461cfa0e9dfbe84
00XAhmedX00 <ahmed14220040@gmail.com>
2025-09-24 22:50:44 +0300

    goto

--- lib/Widgets/go_to.dart
+++ lib/Widgets/go_to.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-goTo({required BuildContext context, required Widget page}) {
+goTo({required BuildContext context, required Widget page , bool routed = false}) {
   Navigator.of(context).pushAndRemoveUntil(
     MaterialPageRoute(builder: (context) => page),
-    (route) => false,
+    (route) => routed,
   );
 }
